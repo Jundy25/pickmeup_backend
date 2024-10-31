6f3afa67e2c84f065e29f5b43fa01d0488063931
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-10-31 19:49:57 +0800

    cors url

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,8 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://192.168.133.124:3000'], // Add your React app's URL
+    'allowed_origins' => ['http://localhost:3000', 'https://backendpickmeup-production.up.railway.app'],
+
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
